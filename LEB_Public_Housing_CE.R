d8def8d88528f68ba8b01b147e67a57469dfa8a4
scdwhite <dlwranger@gmail.com>
2019-06-21 11:51:41 -0400

    Added LEB script

---
+++ LEB_Public_Housing_CE.R
@@ -0,0 +1,170 @@
+#David L. White
+#Date: 05/29/2019
+#Purpose: Processes ESRI Shapefiles and tabular data sets to generate a table of summarized public housing metrics and conservation easement sptial proximity. 
+
+
+library(readr)
+library(dplyr)
+library(rgdal)
+library(sf)
+library(sp)
+library(tidyverse)
+library(units)
+#library(shapefiles)
+#library(raster) prevents DPLYR select from working.
+#library(maptools)
+
+
+
+#----------------------- MAX Section 8 Housing Data by YEAR and Generate a DF by Census Tract ID --------------------------#
+# Data Directory: C:\Users\admin\Box Sync\Default Sync Folder\Projects\NSF_CNH\HUD_Analysis\Section8\LEB_AHD.csv
+
+LEB_AHD <- read.csv("C:/Users/admin/Box Sync/Default Sync Folder/Projects/NSF_CNH/HUD_Analysis/Tables_In/LEB_HUD.csv", stringsAsFactors=FALSE)
+LEB_AHD_MAX <- LEB_AHD %>% group_by(GIS_JOIN) %>% filter(YEAR == max(YEAR)) 
+LEB_AHD_Edit <- LEB_AHD_MAX  %>% select(Tract_Num = GIS_JOIN,YEAR,Sec_8_Reported = NUMBER_REPORTED)
+#Summarize the number of section 8 units as check compare to later.
+sum(LEB_AHD_Edit$Sec_8_Reported,na.rm=T)
+
+#------------------------------------ END -----------------------------------#
+
+
+#-------------------------- Read in the County Census Tract Polygon File --------------------------------------#
+LEB_Census_Poly <- sf:::st_read(dsn = "C:/Users/admin/Box Sync/Default Sync Folder/Projects/NSF_CNH/HUD_Analysis/ShapeFiles_In", layer = "LEB_Census_Tract")
+LEB_Census_Poly$Tract_Num <- as.numeric(as.character(LEB_Census_Poly$GISJOIN2))
+str(LEB_Census_Poly)
+plot(LEB_Census_Poly)
+summary(LEB_Census_Poly)
+attributes(LEB_Census_Poly)
+
+#Removes Geometry - If needed
+#LEB_Shape_table <- LEB_shape
+#st_geometry(LEB_Shape_table) <- NULL
+
+#------------------------------------ END -----------------------------------#
+
+#----------------------------- Merge Section 8 Tabular Data and County Census Tract data via Tract Number ----------------------------#
+
+# Uses SP package to merge --- A bit scary, but you do not have to define the join attribute ------ Assume that since Tract_Num is common between the two data sets that is being used -----
+LEB_Census_AHD <- sp:::merge(LEB_Census_Poly, LEB_AHD_Edit)
+
+# Specifies parameters for shapefile export 
+#LEB_out_shape  = tempfile(pattern = "LEB_AHD_", tmpdir = "C:/Users/admin/Box Sync/Default Sync Folder/Projects/NSF_CNH/HUD_Analysis/CensusTract_2000", fileext = ".shp")
+# Exports to a Shapefile
+sf:::st_write(LEB_Census_AHD, "C:/Users/admin/Box Sync/Default Sync Folder/Projects/NSF_CNH/HUD_Analysis/ShapeFiles_Out/LEB_Census_AHD.shp", delete_layer=TRUE)
+#Plot to Check 
+plot(LEB_Census_AHD[c("Sec_8_Reported")])
+sum(LEB_Census_AHD$Sec_8_Reported,na.rm=T)
+
+
+#----------------------------------- End ------------------------------------#
+
+#------------------------------ Summarize and Merge Geocoded Tax Credit with Section 8 and add back to County Census Tract Poly ----------------------------------#
+
+#Open Geocoded Tax Credit
+LEB_TAX_point <- sf:::st_read(dsn = "C:/Users/admin/Box Sync/Default Sync Folder/Projects/NSF_CNH/HUD_Analysis/ShapeFiles_In", layer = "LEB_Tax_Credit")
+
+#Checks to see if there are any duplicates in USER_HUD_ID_NUMBER, if so, need to examine 
+#further as there may be multiple years that need to be addressed. 
+LEB_TAX_point %>% filter(duplicated(.[["USER_HUD_I"]]))
+
+#Summarize the number of units as check for later.
+summarise(LEB_TAX_point, sum(USER_Low_I))
+
+#Spatial Join between Section 8 (Census Tract) with Geocoded Tax Credit (Address - Point). Runs an Intersect
+LEB_AHD_Tax <- sf:::st_join(LEB_Census_AHD,LEB_TAX_point)
+
+#Export result to a shapefile
+#LEB_out_shape_TAX  = tempfile(pattern = "LEB_AHD_TAX_", tmpdir = "C:/Users/admin/Box Sync/Default Sync Folder/Projects/NSF_CNH/HUD_Analysis/CensusTract_2000", fileext = ".shp")
+# Exports to a Shapefile
+sf::st_write(LEB_AHD_Tax, "C:/Users/admin/Box Sync/Default Sync Folder/Projects/NSF_CNH/HUD_Analysis/ShapeFiles_Out/LEB_AHD_TAX.shp", delete_layer=TRUE)
+#Plot to Check 
+plot(LEB_AHD_Tax[c("Sec_8_Reported")])
+plot(LEB_AHD_Tax[c("USER_Numbe")])
+
+#The Spatial Join results in multiple points in a census tract. Need to summarize by census tract
+LEB_AHD_Tax_Sum <- LEB_AHD_Tax %>% group_by(Tract_Num) %>% summarise(Tax_Units_reported = sum(USER_Low_I))
+LEB_AHD_Tax_Edit <- LEB_AHD_Tax  %>% select(Tract_Num,Sec_8_Reported) %>% distinct(Tract_Num, .keep_all = TRUE)
+
+#Generate tabular data. Drops geometry
+st_geometry(LEB_AHD_Tax_Sum) <- NULL
+#Summarize the number of units as check compare to earlier sum.
+sum(LEB_AHD_Tax_Sum$Tax_Units_reported,na.rm=T)
+
+#Generate tabular data. Drops geometry
+st_geometry(LEB_AHD_Tax_Edit) <- NULL
+
+#Run a join
+LEB_AHD_Tax_tabular_join <- inner_join(LEB_AHD_Tax_Edit,LEB_AHD_Tax_Sum, by="Tract_Num")
+#Summarize the number of units as check compare to earlier sum.
+sum(LEB_AHD_Tax_tabular_join$Tax_Units_reported,na.rm=T)
+sum(LEB_AHD_Tax_tabular_join$Sec_8_Reported,na.rm=T)
+
+
+#Spatial Join between Section 8 (Census Tract) with Geocoded Tax Credit (Address - Point). Runs an Intersect
+LEB_AHD_Tax_spatial_join <- sp:::merge(LEB_Census_Poly,LEB_AHD_Tax_tabular_join)
+LEB_AHD_Tax_spatial_join <- st_transform(LEB_AHD_Tax_spatial_join, "+proj=longlat +ellps=GRS80 +datum=NAD83 +no_defs")
+# <- st_cast(LEB_AHD_Tax_spatial_join, "MULTIPOLYGON")
+str(LEB_AHD_Tax_spatial_join)
+st_crs(LEB_AHD_Tax_spatial_join)
+class(LEB_AHD_Tax_spatial_join)
+
+plot(LEB_AHD_Tax_spatial_join[c("Sec_8_Reported")])
+plot(LEB_AHD_Tax_spatial_join[c("Tax_Units_reported")])
+ggplot() + geom_sf(data = LEB_AHD_Tax_spatial_join)
+
+#----------------------------------- End ------------------------------------#
+
+#------------------------------ Merge Geocoded Tax Credit & Section 8 with CE ----------------------------------#
+
+# ---------------------- Geodatabase Feature Layer Import Resulted in the Wrong Data Type ----------------------#
+#gdb <- "C:/Users/admin/Box Sync/Default Sync Folder/Projects/NSF_CNH/Papers/NCED/NCED_CUCED_2018.gdb"
+#LEB_CE_poly <- st_read(gdb, layer = "LEB_CE_data")
+#LEB_CE_poly <- st_transform(LEB_CE_poly, "+proj=longlat +ellps=GRS80 +datum=NAD83 +no_defs")
+#ggplot() + geom_sf(data = LEB_CE_poly)
+#LEB_CE_poly <- st_cast(LEB_CE_poly, "MULTIPOLYGON")
+
+# --------------------- Use of Shapefile Results in Correct Data Type "Multipolygon" ------------------------#
+LEB_CE_poly <- st_read("C:/Users/admin/Box Sync/Default Sync Folder/Projects/NSF_CNH/HUD_Analysis/ShapeFiles_In/LEB_CE_data.shp")
+LEB_CE_poly <- st_transform(LEB_CE_poly, "+proj=longlat +ellps=GRS80 +datum=NAD83 +no_defs")
+LEB_CE_poly_select <- LEB_CE_poly %>% select(geometry)
+ggplot() + geom_sf(data = LEB_CE_poly)
+str(LEB_CE_poly)
+st_crs(LEB_CE_poly)
+class(LEB_CE_poly)
+
+#----------------------------------- End ------------------------------------#
+
+
+# ------------------------- SF Intersects Builds a list of polygons that have CEs --------------------------#
+LEB_AHD_Tax_CE_spatial_join_list <- sf:::st_intersects(LEB_AHD_Tax_spatial_join,LEB_CE_poly_select, sparse = TRUE)
+#Convert the list to a DF
+LEB_AHD_Tax_CE_spatial_join_df <- as.data.frame(LEB_AHD_Tax_CE_spatial_join_list)
+
+# --------------------------- Creates a vector based on row numbers from intersect performed in prior step -----------------------#
+poly_list <- LEB_AHD_Tax_CE_spatial_join_df %>% distinct(LEB_AHD_Tax_CE_spatial_join_df$row.id, row.id = LEB_AHD_Tax_CE_spatial_join_df$row.id)
+#Generate a new list of only row.id. Yes I know there are easier ways to do this. :-)
+poly_list <- as.vector(poly_list$row.id) 
+
+# ---------------------------- Adds a CE_Present column and Row.ID column that matches poly_list ---------------------#
+LEB_AHD_Tax_spatial_join <- mutate(LEB_AHD_Tax_spatial_join, CE_Present = 0, row.id = as.numeric(rownames(LEB_AHD_Tax_spatial_join)))
+
+# --------------------------- DPLYR to populate a binary column in the original spatial join. Are CEs present... yes = 1 & no = 0 ------#
+LEB_AHD_Tax_spatial_join_FINAL <- LEB_AHD_Tax_spatial_join %>% mutate(CE_Present = replace(CE_Present,row.id %in% poly_list, 1))
+ggplot() + geom_sf(data = LEB_AHD_Tax_spatial_join_FINAL)
+
+#Export result to a shapefile
+#LEB_AHD_Tax_spatial_join_FINAL_shp  = tempfile(pattern = "LEB_AHD_Tax_spatial_join_FINAL_", tmpdir = "C:/Users/admin/Box Sync/Default Sync Folder/Projects/NSF_CNH/HUD_Analysis/CensusTract_2000", fileext = ".shp")
+# Exports to a Shapefile
+sf::st_write(LEB_AHD_Tax_spatial_join_FINAL, "C:/Users/admin/Box Sync/Default Sync Folder/Projects/NSF_CNH/HUD_Analysis/ShapeFiles_Out/LEB_AHD_Tax_spatial_join_FINAL.shp",  delete_layer=TRUE)
+
+#Export data frame only
+LEB_AHD_Tax_spatial_join_FINAL_table <- dplyr::select(as.data.frame(LEB_AHD_Tax_spatial_join_FINAL), -geometry, -row.id)
+write.csv(LEB_AHD_Tax_spatial_join_FINAL_table, "C:/Users/admin/Box Sync/Default Sync Folder/Projects/NSF_CNH/HUD_Analysis/FinalTables_Out/LEB_AHD_Tax_spatial_join_FINAL_table.csv" )
+
+#Drop geometry
+st_geometry(LEB_AHD_Tax_spatial_join_FINAL) <- NULL
+#Summarize the number of units as check compare to earlier sum.
+sum(LEB_AHD_Tax_tabular_join$Tax_Units_reported,na.rm=T)
+sum(LEB_AHD_Tax_tabular_join$Sec_8_Reported,na.rm=T)
+
+#----------------------------------- End Of Script ------------------------------------#
