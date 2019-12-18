5792df649c822facc769b77ddc04d9106625cb94
whitedl <whitedl@clemson.edu>
2019-12-18 14:26:32 -0500

    Update DGL_Public_Housing_CE.R
    Working to add Time component

--- DGL_Public_Housing_CE.R
+++ DGL_Public_Housing_CE.R
@@ -17,19 +17,20 @@ library(units)
 
 
 #----------------------- MAX Section 8 Housing Data by YEAR and Generate a DF by Census Tract ID --------------------------#
-# Data Directory: C:\Users\admin\Box Sync\Default Sync Folder\Projects\NSF_CNH\HUD_Analysis\Section8\DGL_AHD.csv
+# Data Directory: C:\Users\whitedl\Box Sync\Default Sync Folder\Projects\NSF_CNH\HUD_Analysis\Section8\DGL_AHD.csv
 
-DGL_AHD <- read.csv("C:/Users/admin/Box Sync/Default Sync Folder/Projects/NSF_CNH/HUD_Analysis/Tables_In/DGL_HUD.csv", stringsAsFactors=FALSE)
+DGL_AHD <- read.csv("C:/Users/whitedl/Box Sync/Default Sync Folder/Projects/NSF_CNH/HUD_Analysis/Tables_In/DGL_HUD.csv", stringsAsFactors=FALSE)
 DGL_AHD_MAX <- DGL_AHD %>% group_by(GIS_JOIN) %>% filter(YEAR == max(YEAR)) 
-DGL_AHD_Edit <- DGL_AHD_MAX  %>% select(Tract_Num = GIS_JOIN,YEAR,Sec_8_Reported = NUMBER_REPORTED)
+DGL_AHD_Edit <- DGL_AHD_MAX %>% drop_na(YEAR) %>% select(Tract_Num = GIS_JOIN,YEAR,Sec_8_Reported = NUMBER_REPORTED)
 #Summarize the number of section 8 units as check compare to later.
+
 sum(DGL_AHD_Edit$Sec_8_Reported,na.rm=T)
 
 #------------------------------------ END -----------------------------------#
 
 
 #-------------------------- Read in the County Census Tract Polygon File --------------------------------------#
-DGL_Census_Poly <- sf:::st_read(dsn = "C:/Users/admin/Box Sync/Default Sync Folder/Projects/NSF_CNH/HUD_Analysis/ShapeFiles_In", layer = "DGL_Census_Tract")
+DGL_Census_Poly <- sf:::st_read(dsn = "C:/Users/whitedl/Box Sync/Default Sync Folder/Projects/NSF_CNH/HUD_Analysis/ShapeFiles_In", layer = "DGL_Census_Tract")
 DGL_Census_Poly$Tract_Num <- as.numeric(as.character(DGL_Census_Poly$GISJOIN2))
 str(DGL_Census_Poly)
 plot(DGL_Census_Poly)
@@ -48,9 +49,9 @@ attributes(DGL_Census_Poly)
 DGL_Census_AHD <- sp:::merge(DGL_Census_Poly, DGL_AHD_Edit)
 
 # Specifies parameters for shapefile export 
-#DGL_out_shape  = tempfile(pattern = "DGL_AHD_", tmpdir = "C:/Users/admin/Box Sync/Default Sync Folder/Projects/NSF_CNH/HUD_Analysis/CensusTract_2000", fileext = ".shp")
+#DGL_out_shape  = tempfile(pattern = "DGL_AHD_", tmpdir = "C:/Users/whitedl/Box Sync/Default Sync Folder/Projects/NSF_CNH/HUD_Analysis/CensusTract_2000", fileext = ".shp")
 # Exports to a Shapefile
-sf:::st_write(DGL_Census_AHD, "C:/Users/admin/Box Sync/Default Sync Folder/Projects/NSF_CNH/HUD_Analysis/ShapeFiles_Out/DGL_Census_AHD.shp", delete_layer=TRUE)
+sf:::st_write(DGL_Census_AHD, "C:/Users/whitedl/Box Sync/Default Sync Folder/Projects/NSF_CNH/HUD_Analysis/ShapeFiles_Out/DGL_Census_AHD.shp", delete_layer=TRUE)
 #Plot to Check 
 plot(DGL_Census_AHD[c("Sec_8_Reported")])
 sum(DGL_Census_AHD$Sec_8_Reported,na.rm=T)
@@ -59,14 +60,14 @@ sum(DGL_Census_AHD$Sec_8_Reported,na.rm=T)
 #----------------------------------- End ------------------------------------#
 
 # ---------------------- Geodatabase Feature Layer Import Resulted in the Wrong Data Type ----------------------#
-#gdb <- "C:/Users/admin/Box Sync/Default Sync Folder/Projects/NSF_CNH/Papers/NCED/NCED_CUCED_2018.gdb"
+#gdb <- "C:/Users/whitedl/Box Sync/Default Sync Folder/Projects/NSF_CNH/Papers/NCED/NCED_CUCED_2018.gdb"
 #DGL_CE_poly <- st_read(gdb, layer = "DGL_CE_data")
 #DGL_CE_poly <- st_transform(DGL_CE_poly, "+proj=longlat +ellps=GRS80 +datum=NAD83 +no_defs")
 #ggplot() + geom_sf(data = DGL_CE_poly)
 #DGL_CE_poly <- st_cast(DGL_CE_poly, "MULTIPOLYGON")
 
 # --------------------- Use of Shapefile Results in Correct Data Type "Multipolygon" ------------------------#
-DGL_CE_poly <- st_read("C:/Users/admin/Box Sync/Default Sync Folder/Projects/NSF_CNH/HUD_Analysis/ShapeFiles_In/DGL_CE_data.shp")
+DGL_CE_poly <- st_read("C:/Users/whitedl/Box Sync/Default Sync Folder/Projects/NSF_CNH/HUD_Analysis/ShapeFiles_In/DGL_CE_data.shp")
 DGL_CE_poly <- st_transform(DGL_CE_poly, "+proj=longlat +ellps=GRS80 +datum=NAD83 +no_defs")
 DGL_CE_poly_select <- DGL_CE_poly %>% select(geometry)
 ggplot() + geom_sf(data = DGL_CE_poly)
@@ -97,13 +98,13 @@ DGL_Census_AHD_FINAL <- DGL_Census_AHD %>% mutate(CE_Present = replace(CE_Presen
 ggplot() + geom_sf(data = DGL_Census_AHD_FINAL)
 
 #Export result to a shapefile
-#DGL_AHD_Tax_spatial_join_FINAL_shp  = tempfile(pattern = "DGL_AHD_Tax_spatial_join_FINAL_", tmpdir = "C:/Users/admin/Box Sync/Default Sync Folder/Projects/NSF_CNH/HUD_Analysis/CensusTract_2000", fileext = ".shp")
+#DGL_AHD_Tax_spatial_join_FINAL_shp  = tempfile(pattern = "DGL_AHD_Tax_spatial_join_FINAL_", tmpdir = "C:/Users/whitedl/Box Sync/Default Sync Folder/Projects/NSF_CNH/HUD_Analysis/CensusTract_2000", fileext = ".shp")
 # Exports to a Shapefile
-sf::st_write(DGL_Census_AHD_FINAL, "C:/Users/admin/Box Sync/Default Sync Folder/Projects/NSF_CNH/HUD_Analysis/ShapeFiles_Out/DGL_AHD_Tax_spatial_join_FINAL.shp",  delete_layer=TRUE)
+sf::st_write(DGL_Census_AHD_FINAL, "C:/Users/whitedl/Box Sync/Default Sync Folder/Projects/NSF_CNH/HUD_Analysis/ShapeFiles_Out/DGL_AHD_Tax_spatial_join_FINAL.shp",  delete_layer=TRUE)
 
 #Export data frame only
 DGL_Census_AHD_FINAL_table <- dplyr::select(as.data.frame(DGL_Census_AHD_FINAL), -geometry, -row.id)
-write.csv(DGL_Census_AHD_FINAL_table, "C:/Users/admin/Box Sync/Default Sync Folder/Projects/NSF_CNH/HUD_Analysis/FinalTables_Out/DGL_AHD_Tax_spatial_join_FINAL_table.csv" )
+write.csv(DGL_Census_AHD_FINAL_table, "C:/Users/whitedl/Box Sync/Default Sync Folder/Projects/NSF_CNH/HUD_Analysis/FinalTables_Out/DGL_AHD_Tax_spatial_join_FINAL_table.csv" )
 
 #Drop geometry
 st_geometry(DGL_Census_AHD_FINAL) <- NULL
